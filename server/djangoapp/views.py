07c82f198cb3f9575415f684593287895e578df4
Vanessa Yucabeth <vanessayucabeth@gmail.com>
2025-08-07 19:02:39 -0400

    Add CarMake and CarModel models with dealer_id support, resolve IntegrityError, and load sample car data into the database

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
+from .models import CarMake, CarModel
+
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -81,6 +83,20 @@ def registration(request):
         return JsonResponse({"userName": username, "status": "Authenticated"})
     else:
         return JsonResponse({"userName": username, "error": "Already Registered"})
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if count == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+    return JsonResponse({"CarModels": cars})
 
 
 # # Update the `get_dealerships` view to render the index page with
