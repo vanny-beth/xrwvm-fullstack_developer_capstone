1df61a47838d68ba2aea95fe9b6398528897780b
Vanessa Yucabeth <vanessayucabeth@gmail.com>
2025-10-05 07:36:50 -0400

    🚀 Successful deployment: app running on Kubernetes with image pull secret

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,4 +4,4 @@ echo "Making migrations and migrating the database."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec "$@"
+exec gunicorn djangoproj.wsgi:application --bind 0.0.0.0:8000
